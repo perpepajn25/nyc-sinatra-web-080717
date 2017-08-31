653b6371d29d9bfa919063898974df53dbd5d7b1
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-31 10:22:16 -0400

    test migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,28 +13,28 @@
 ActiveRecord::Schema.define(version: 20170831140012) do
 
   create_table "figure_titles", force: :cascade do |t|
-    t.integer  "title_id"
-    t.integer  "figure_id"
+    t.integer "title_id"
+    t.integer "figure_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "figures", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "landmarks", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "figure_id"
-    t.integer  "year_completed"
+    t.string "name"
+    t.integer "figure_id"
+    t.integer "year_completed"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "titles", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
